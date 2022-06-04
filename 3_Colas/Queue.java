7e7b3f54f93ae5a2533e7961b69a62dc15e2bd0b
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-06-04 11:59:57 -0500

    Creando plantilla de la clase Queue

---
+++ 3_Colas/Queue.java
@@ -0,0 +1,24 @@
+public class Queue<E> {
+  private Node <E> root;
+  private Node <E> last;
+  private final int size;
+  private int numElementos = -1;//cuenta el numero de elementos actuales
+
+  public Queue(int n){
+    this.root = null;
+    this.last = null;
+    this.size = n;
+  }
+  public boolean offer(E e){
+  }
+  public boolean add(E e){
+  }
+  public E peek(){
+  }
+  public E element(){
+  }
+  public E poll(){
+  }
+  public E remove(){
+  }
+}
