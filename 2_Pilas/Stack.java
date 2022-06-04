48362a5619c9acaf657dddee550bbb752c0a9238
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-06-03 19:13:56 -0500

    Agregando método push en Stack y testeándolo en la clase Test

--- 2_Pilas/Stack.java
+++ 2_Pilas/Stack.java
@@ -7,4 +7,9 @@ public class Stack<T> {
 		this.root = null;
 	}
 
+	public void push(T x) {
+		// Cada vez que usemos un push colocamos al elemento en la parte de arriba
+		this.root = new Node<T>(x,this.root); 
+	}
+
 }
--- 2_Pilas/Test.java
+++ 2_Pilas/Test.java
@@ -2,6 +2,27 @@
 public class Test {
 
 	public static void main(String[] args) {
-		
+		// USAMOS LA CLASE INTEGER DE JAVA PARA ESTE TEST
+		// LA PILA IMPLEMENTADA PUEDE SER USADA INCLUSO CON CLASES QUE NO SEAN PREDETERMINADAS DE JAVA
+		Stack<Integer> stack = new Stack<Integer>();
+
+		// PUSH: AGREGAMOS ELEMENTOS A NUESTRA PILA
+		// Los últimos elementos en agregarse serán los primeros
+		stack.push(23);
+		System.out.println(stack);
+		stack.push(15);
+		System.out.println(stack);
+		stack.push(2);
+		System.out.println(stack);
+		stack.push(11);
+		System.out.println(stack);
+		stack.push(17);
+		System.out.println(stack);
+		stack.push(26);
+		System.out.println(stack);
+		stack.push(3);
+		System.out.println(stack);
+		stack.push(14);
+		System.out.println(stack);
 	}
 }
