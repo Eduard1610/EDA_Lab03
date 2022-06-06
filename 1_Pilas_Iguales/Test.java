5d2a778ca092029f659743029d41515ed220c43b
M-UNSA <82843764+M-UNSA@users.noreply.github.com>
2022-06-05 23:28:15 -0500

    Testing toString

--- 1_Pilas_Iguales/Test.java
+++ 1_Pilas_Iguales/Test.java
@@ -1,9 +1,28 @@
 public class Test{
 
-  public static void main(String[] args){
+    public static void main(String[] args){
+        Stack h1 = new Stack();
+        Stack h2 = new Stack();
+        Stack h3 = new Stack();
 
+        h1.push(1);
+        h1.push(1);
+        h1.push(1);
+        h1.push(2);
+        h1.push(3);
 
+        h2.push(2);
+        h2.push(3);
+        h2.push(4);
 
-  }
+        h3.push(1);
+        h3.push(4);
+        h3.push(1);
+        h3.push(1);
+
+        System.out.println("H1 contiene: "+h1);
+        System.out.println("H2 contiene: "+h2);
+        System.out.println("H3 contiene: "+h3);
+    }
 
  }
